60eaf2d399c79f374680f0d41af6bcfc9e7ad23b
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-08-08 19:19:20 +0200

    form structure and input

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <div className="top__details">
+      <h1>IP  Address Tracker</h1>
+
+      <form action="" className="input-form">
+        <input type="text" id="input" name="input" className="input-field"/>
+        <button></button>
+      </form>
+    </div>
+    <div className="map__details">
 
     </div>
+    </>
   );
 }
 
