fff0e0cd3cb46d15b50fbad8ae647a56a0e8bb06
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2022-08-08 19:54:35 +0200

    input form created

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { HiOutlineChevronRight } from "react-icons/hi";
+import "./App.css";
 
 function App() {
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
   return (
     <>
-    <div className="top__details">
-      <h1>IP  Address Tracker</h1>
+      <div className="top__details">
+        <h1 className="heading">IP Address Tracker</h1>
 
-      <form action="" className="input-form">
-        <input type="text" id="input" name="input" className="input-field"/>
-        <button></button>
-      </form>
-    </div>
-    <div className="map__details">
-
-    </div>
+        <form action="" className="input-form" onSubmit={onSubmit}>
+          <div className="input-control">
+            <input
+              type="text"
+              id="input"
+              name="input"
+              className="input-field"
+            />
+            <button className="btn">
+              <HiOutlineChevronRight />
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="map__details"></div>
     </>
   );
 }
